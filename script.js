6506f4ef2cce2dc49e03ca267e786d9ade4f9b45
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-05-25 21:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+let input = document.getElementById('rope-length');
+let ans = document.getElementById('result');
+
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  let sum = 0;
+	let arr = [];
+	let str = input.value.split(',');
+	for(let num of str){
+		arr.push(parseInt(num));
+	}
+
+	while(arr.length > 1){
+		arr.sort(function(x,y)){
+			return a - b;
+		}
+		let merge = arr[0] + arr[1];
+		sum += merge;
+		arr.splice(0,2);
+		arr.push(merge);
+	}
+    ans.innerText = sum;
 }  
